8ede7a91f52bac26354db3cfd0c9f95701a796cf
egor <egor.prishchepin@gmail.com>
2015-03-31 22:35:26 +0300

    init

---
+++ MyAppl/app/src/main/java/com/example/egor/myappl/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.egor.myappl;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.HashMap;
+
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button mTranslateButton;
+    private Button mSwitchLangButton;
+    private TextView mTranslatedText;
+    private String direction;
+    private HashMap<String, String> languages;
+    private EditText mEditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        mTranslateButton = (Button) view.findViewById(R.id.translate_button);
+        mTranslatedText = (TextView)view.findViewById(R.id.hello);
+        mTranslateButton.setOnClickListener(this);
+
+
+    }
+
+
+    public void onClick(View v) {
+        System.out.println("fasfasf");
+        if(v == mTranslateButton) {
+           // mTranslatedText.setText("213");
+            System.out.println("fasfasf");
+        }
+
+    }
+
+}
